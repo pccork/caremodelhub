c4ef7c4d5e4bba177127089bd9c3ff33c551d725
pccork <petercmchuk@gmail.com>
2026-02-07 15:45:26 +0000

    fix (frontend) set specific Render hostname in Vite preview

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -2,8 +2,10 @@
 
 import axios from "axios";
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:4000",
+  baseURL: API_URL,
   timeout: 15000,
 });
 
